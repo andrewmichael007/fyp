b7df53f552e0cc9081a88de1b8817098749032c1
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-17 21:05:40 +0000

    initial implementation of soil moisture sensor (source file)

--- main/src/sensors/soil_moisture_sensor.cpp
+++ main/src/sensors/soil_moisture_sensor.cpp
@@ -1,1 +1,31 @@
+#include "soil_moisture_sensor.h"
+#include <Arduino.h>
 
+
+#define MOISTURE_PIN 34
+
+bool SoilMoistureSensor::begin()
+{
+    pinMode(MOISTURE_PIN, INPUT);
+    return true;
+}
+
+int SoilMoistureSensor::readRaw()
+{
+    return analogRead(MOISTURE_PIN);
+}
+
+//calculation of the soil moisture percentage
+
+// current position in range / total range * 100
+float SoilMoistureSensor::readPercentage()
+{
+    int raw = readRaw();
+
+    float percentage =
+        ((float)(dryValue - raw) / (dryValue - wetValue)) * 100.0;
+
+    percentage = constrain(percentage, 0, 100);
+
+    return percentage;                                                                                                                                                                                                                                                                                              
+}
