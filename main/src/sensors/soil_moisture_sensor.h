1cb8ba8c9e4bd6db6a78432cdfeaf9ab06f3029e
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-17 21:07:27 +0000

    added the header file for source file

--- main/src/sensors/soil_moisture_sensor.h
+++ main/src/sensors/soil_moisture_sensor.h
@@ -1,1 +1,19 @@
+#ifndef SOIL_MOISTURE_SENSOR_H
+#define SOIL_MOISTURE_SENSOR_H
 
+// creating a blueprint for soil moisture sensor
+class SoilMoistureSensor
+{
+public:
+    bool begin();
+
+    int readRaw();
+
+    float readPercentage();
+
+private:
+    const int dryValue = 3500;
+    const int wetValue = 1500;
+};
+
+#endif
