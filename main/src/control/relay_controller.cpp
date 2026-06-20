3b66417f60094c8bbadb9fe418c09ba105e3ae5d
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-20 23:55:18 +0000

    this file shows the actions of the relay that can be carried out

---
+++ main/src/control/relay_controller.cpp
@@ -0,0 +1,38 @@
+//this source file shows how the actions of the relay controller would be carried out
+
+#include "relay_controller.h"
+#include <Arduino.h>
+
+#define PUMP_RELAY_PIN 26
+#define VALVE_RELAY_PIN 27
+
+
+bool RelayController::begin()
+{
+    pinMode(PUMP_RELAY_PIN, OUTPUT);
+    pinMode(VALVE_RELAY_PIN, OUTPUT);
+
+    stopIrrigation();
+
+    return true;
+}
+
+void RelayController::startIrrigation()
+{
+    digitalWrite(PUMP_RELAY_PIN, HIGH);
+    digitalWrite(VALVE_RELAY_PIN, HIGH);
+}
+
+void RelayController::stopIrrigation()
+{
+    digitalWrite(PUMP_RELAY_PIN, LOW);
+    digitalWrite(VALVE_RELAY_PIN, LOW);
+}
+
+//note: check the relays
+
+// 1. can be active = low;
+// meaning : high = off & low = on
+
+// 2. can be active = high
+// meaning : high = on & low = off
