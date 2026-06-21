ba34dad79e7cd4ba790aaeac741c7e0c28cb057f
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-21 00:53:43 +0000

    this is the main implementation of the irrigation system

--- main/src/control/irrigation_controller.cpp
+++ main/src/control/irrigation_controller.cpp
@@ -2,11 +2,22 @@
 
 IrrigationDecision IrrigationController::evaluate( const SensorData& data)
 {
+    // priority 1:
+    // emergency protection
+
+    if(data.soilMoisture < 15)
+    {
+        return IrrigationDecision::CRITICAL_IRRIGATION;
+    }
+
+    // priority 2:
+    // context-aware irrigation
+
     if(data.soilMoisture < 30 &&
        data.temperature > 30 &&
        data.humidity < 50)
     {
-        return IrrigationDecision::IRRIGATE;
+        return IrrigationDecision::CONTEXT_IRRIGATION;
     }
 
     return IrrigationDecision::DO_NOT_IRRIGATE;
