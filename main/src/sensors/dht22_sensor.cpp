91f551151807cf7ea03b05debce13db44c5e1d40
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-22 10:07:35 +0000

    temperature and humidity sensor implementation

--- main/src/sensors/dht22_sensor.cpp
+++ main/src/sensors/dht22_sensor.cpp
@@ -2,24 +2,23 @@
 #include <Arduino.h>
 #include <DHT.h>
 
-
 #define DHT_PIN 4
 #define DHT_TYPE DHT22
 
 static DHT dht(DHT_PIN, DHT_TYPE);
 
-bool DHTSensor::begin()
+bool DHT22Sensor::begin()
 {
     dht.begin();
     return true;
 }
 
-float DHTSensor::readTemperature()
+float DHT22Sensor::readTemperature()
 {
     return dht.readTemperature();
 }
 
-float DHTSensor::readHumidity()
+float DHT22Sensor::readHumidity()
 {
     return dht.readHumidity();
 }
