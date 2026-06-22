cb15514da3965dd150dbbfeda1afaf4905f72306
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-22 12:25:28 +0000

    the header file for managing all four sensors

--- main/src/sensors/sensor_manager.h
+++ main/src/sensors/sensor_manager.h
@@ -1,1 +1,25 @@
+// this is the sensor manager, it manages all sensors
+#ifndef SENSOR_MANAGER_H
+#define SENSOR_MANAGER_H
+
+// we include these files in other to manage the sensors
+#include "sensor_data.h"
+#include "dht22_sensor.h"
+#include "soil_moisture_sensor.h"
+#include "bh1750_sensor.h"
+
+class SensorManager
+
+{
+public:
+    bool begin();
+    SensorData getSensorData();
+
+private:
+    DHT22Sensor dht22Sensor;
+    SoilMoistureSensor SoilMoistureSensor;
+    BH1750Sensor bh1750Sensor;
+};
+
+#endif
 
