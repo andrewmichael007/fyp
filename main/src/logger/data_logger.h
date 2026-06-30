62039c5bcbd6b157e561f0913fa12cd444458f10
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-30 18:30:31 +0000

    this is logger file for the implementation of the cpp file which generates the csv file

--- main/src/logger/data_logger.h
+++ main/src/logger/data_logger.h
@@ -1,22 +1,32 @@
 //this data logger file generates the csv file (dataset) for machine learning implementation
+// uses the log record as input and outputs a csv file
 #ifndef DATA_LOGGER_H
 #define DATA_LOGGER_H
 
-#include "../sensors/sensor_data.h"
-#include "../control/irrigation_decision.h"
-#include "../system/system_state.h"
+#include <Arduino.h>
+
+//we  include  the log record file because that's what we are using 
+#include "log_record.h"
 
 class DataLogger
 {
 public:
-    static void log(
-        const SensorData& data,
-        IrrigationDecision decision,
+
+    static String buildCSV(
+        const LogRecord& record
+    );
+
+private:
+
+    static const char*
+    decisionToString(
+        IrrigationDecision decision
+    );
+
+    static const char*
+    stateToString(
         SystemState state
     );
 };
 
 #endif
-
-
-
